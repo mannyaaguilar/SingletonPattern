1497a6ec65cf232b1e2ceb5f0ec41c7858e10959
Manny Aguilar <mannyaaguilar@yahoo.com>
2019-01-27 02:53:56 -0700

    first commit

---
+++ src/com/manny/Logger.java
@@ -0,0 +1,18 @@
+package com.manny;
+
+public class Logger {
+
+    private static Logger logger;
+
+    private Logger(){
+
+    }
+
+    public static Logger getInstance(){
+
+        if(logger == null){
+            logger = new Logger();
+        }
+        return logger;
+    }
+}
